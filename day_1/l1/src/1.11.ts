994acb79d98c2673dac441e4f3d006980f5f5a3e
showrov4g <showrovg00@gmail.com>
2025-05-03 09:47:26 +0600

    typescript first code run

---
+++ day_1/l1/src/1.11.ts
@@ -0,0 +1,10 @@
+{
+    // ternary operator / optional chaining / nullish coalescing operator
+    let age: number = 20;
+
+    if (age >= 18) {
+        console.log("Adult")
+    } else {
+        console.log("minor")
+    }
+}
