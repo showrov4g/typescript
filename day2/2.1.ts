16f0fa495c11102d2985d7cbc97ffd0afbf19d6d
showrov4g <showrovg00@gmail.com>
2025-05-03 11:10:20 +0600

    learning typescript"

--- day2/2.1.ts
+++ day2/2.1.ts
@@ -1,19 +1,23 @@
 {
     //type assertion
 
-    let anything : any ;
+    let anything: any;
     anything = "next label web developer";
-    anything = 333 ;
-//    (anything as number);
-    const kgToGram = (value: number | string) =>{
-        if (typeof value === "string"){
-            const convertedValue = parseFloat(value)
-            return convertedValue;
+    anything = 333;
+    //    (anything as number);
+    const kgToGram = (value: number | string): string | number |undefined => {
+        if (typeof value === "string") {
+            const convertedValue = parseFloat(value) * 1000
+            return `the converted value is : ${convertedValue}`;
         }
-        if(typeof value === "number"){
-            
+        if (typeof value === "number") {
+            return value
         }
     }
+
+    const result1 = kgToGram(1000) as number; 
+    console.log(result1)
+    
 
 
 }
