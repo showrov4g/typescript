5a2ca084f56114b6b087dec8bb0ad226b36c9eb9
showrov4g <showrovg00@gmail.com>
2025-05-04 09:01:31 +0600

    learning typescript

--- day2/function.ts
+++ day2/function.ts
@@ -1,3 +1,22 @@
 {
-    
+    const createArray = (param: string): string[] => {
+        return [param];
+    }
+    const createArrayWithGeneric = <T>(param: T): T[] => {
+        return [param];
+    }
+
+    const result1 = createArray("bangladesh")
+    type User = {
+        id: number,
+        name: string,
+        class: number
+    }
+    const resGen = createArrayWithGeneric<User>({
+        id: 123,
+        name:"Ghosh",
+        class: 6,
+
+    })
+
 }
