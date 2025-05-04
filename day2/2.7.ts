dd452bdb7fa74d91322d650b89e654d2d462d612
showrov4g <showrovg00@gmail.com>
2025-05-04 10:27:39 +0600

    learning typescript

--- day2/2.7.ts
+++ day2/2.7.ts
@@ -1,7 +1,15 @@
 {
     //generic constrains with keyof  operator;
 
-    
+    type vehicles ={
+        bike: string,
+        car: string,
+        ship: string,
+    }
+
+    type Owner = {
+        
+    }
 
     //
 }
