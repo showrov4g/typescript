385a6097c4d6436aa523dd1fe8f060eae032d2f0
showrov4g <showrovg00@gmail.com>
2025-05-04 09:32:07 +0600

    learnging ts

--- day2/function.ts
+++ day2/function.ts
@@ -6,6 +6,12 @@
         return [param];
     }
 
+    const createArrayWithTuple = <T,Q>(param1: T, param2: Q) : [T,Q] =>{
+        return [param1 , param2];
+    }
+
+
+
     const result1 = createArray("bangladesh")
     type User = {
         id: number,
