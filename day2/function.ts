39e2f7fbd49752d7f1f75d82054e46fb23c72f6c
showrov4g <showrovg00@gmail.com>
2025-05-04 10:02:38 +0600

    learning TypeScript

--- day2/function.ts
+++ day2/function.ts
@@ -26,4 +26,19 @@
 
     })
 
+    const course =<T>(student: T)=>{
+        const studentCourse = "next label Web development course"
+        return {
+            ...student,
+            studentCourse
+        }
+    }
+
+    const student1 = course({
+        name: "sk",
+        email: "abc@gmial.com",
+        roll: 12,
+        devType: "new developer"
+    }) 
+
 }
