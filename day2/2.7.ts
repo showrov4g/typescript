52bf5b89ec9a5a23596bd756851a9f32982cf57f
showrov4g <showrovg00@gmail.com>
2025-05-04 11:06:18 +0600

    learning typescript

--- day2/2.7.ts
+++ day2/2.7.ts
@@ -1,15 +1,31 @@
 {
     //generic constrains with keyof  operator;
 
-    type vehicles ={
+    type vehicles = {
         bike: string,
         car: string,
         ship: string,
     }
 
-    type Owner = {
-        
+    type Owner = "bike" | "car" | "ship";
+    type Owner2 = keyof vehicles;
+
+    const person1 : Owner2 = "bike"
+
+
+    //functions 
+
+    const getPropertyValue =<X, Y extends "name" | "age"| "address">(obj:X, key: Y)=>{
+        return obj[key]
     }
+
+    const user = {
+        name: "ghosh",
+        age:35,
+        address: "Jheniadh"
+    }
+
+    const result1 = getPropertyValue(user,"age")
 
     //
 }
