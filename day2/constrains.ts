522df4ed8397446991b1b0dd54713de91fe8607f
showrov4g <showrovg00@gmail.com>
2025-05-04 10:04:26 +0600

    learning typescript

--- day2/constrains.ts
+++ day2/constrains.ts
@@ -1,4 +1,18 @@
 {
     //constraints
-    
+    const course =<T>(student: T)=>{
+        const studentCourse = "next label Web development course"
+        return {
+            ...student,
+            studentCourse
+        }
+    }
+
+    const student1 = course({
+        name: "sk",
+        email: "abc@gmial.com",
+        roll: 12,
+        devType: "new developer"
+    }) 
+
 }
