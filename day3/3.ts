f1013b8818bbbb58be8cf0871fbe78a9a3dbdec8
showrov4g <showrovg00@gmail.com>
2025-05-05 11:03:43 +0600

    learning ts"

--- day3/3.ts
+++ day3/3.ts
@@ -1,13 +1,40 @@
 {
     // type gard
+
+        type AlphaNumeric = string | number;
+
+
+
     const add = (
-        param1: string | number,
-        param2: string | number
-    ): string | number => {
+        param1: AlphaNumeric,
+        param2: AlphaNumeric,
+    ): AlphaNumeric => {
         if (typeof param1 === "number" && typeof param2 === "number") {
             return param1 + param2;
         }else{
         return param1.toString() + param2.toString();
         }
     };
+
+    const result1 = add("ghosh", 123);
+    console.log(result1)
+
+    // in gard
+
+    type NormalUser = {
+        name: string;
+    }
+    type AdminUser = {
+        name: string,
+        role: "admin"
+    }
+    const getUser = (user: NormalUser | AdminUser) =>{
+        if("role" in user){
+            user.role
+        }else{
+            
+        }
+    }
+
+
 }
