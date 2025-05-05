737dbdd8798e3bb74c7f444c2f7e4d47702ff72b
showrov4g <showrovg00@gmail.com>
2025-05-05 11:16:25 +0600

    Learnign typescript

--- day3/4.ts
+++ day3/4.ts
@@ -1,3 +1,36 @@
 {
+
+    class Animal {
+        name: string;
+        breed: string;
+        constructor(name: string, breed: string){
+            this.name = name;
+            this.breed = breed;
+        }
+        makeSound(){
+            console.log("this sound is making")
+        }
+    }
+
+    class Dog extends Animal{
+        constructor(name: string, breed: string){
+            super(name, breed)
+        }
+        makeBark(){
+            console.log("It is barking")
+        }
+    }
+    class Cat extends Animal{
+        constructor(name: string, breed: string){
+            super(name, breed)
+        }
+        makeMeow(){
+            console.log("It is barking")
+        }
+    }
+
     
+
+
+
 }
