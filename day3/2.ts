630383c9ae0ae8e1e5bdf56c5e9fc84fc2142760
showrov4g <showrovg00@gmail.com>
2025-05-05 08:39:20 +0600

    Learning typescript

--- day3/2.ts
+++ day3/2.ts
@@ -1,5 +1,17 @@
 {
-    class{
-        
+    class student{
+        name: string;
+        age: number;
+        address: string;
+        constructor(name: string, age: number, address: string){
+            this.name = name;
+            this.age = age;
+            this.address = address
+        }
     }
+    
+
+
+
+
 }
