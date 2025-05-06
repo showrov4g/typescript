16b897bb5a556bf1820e16066b0e5b3dcc2cc72f
showrov4g <showrovg00@gmail.com>
2025-05-06 11:04:24 +0600

    learning ts"

--- day3/7.ts
+++ day3/7.ts
@@ -1,3 +1,15 @@
 {
-    
+
+    class Counter {
+        count: number = 0;
+        increment(){
+            return(this.count = this.count +1)
+        }
+        discernment(){
+            return (this.count = this.count -1);
+        }
+    }
+
+
+
 }
