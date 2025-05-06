cfaf8e39001d53ee23421b834e4f0d99a1d66bc9
showrov4g <showrovg00@gmail.com>
2025-05-06 10:25:06 +0600

    earning typescript

--- day3/5.ts
+++ day3/5.ts
@@ -1,13 +1,24 @@
 {
     class BankAccount {
-        id: number;
-        name: string;
-        balance: number;
+        public readonly id: number;
+        public name: string;
+        private _balance: number;
         constructor(id: number, name: string, balance: number){
             this.id = id;
             this.name = name;
-            this.balance = balance;
-
+            this._balance = balance;
+        }
+        addDeposit(amount: number) {
+            this._balance = this._balance + amount;
+        }
+        getBalance(){
+            return this._balance;
         }
     }
+    const goribManush = new BankAccount(111, "ghoish", 20)
+    goribManush.addDeposit(30)
+   const myBalance = goribManush.getBalance();
+   console.log(myBalance);
+  
+
 }
